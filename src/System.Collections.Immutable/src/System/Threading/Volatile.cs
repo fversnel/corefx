036197f67a98c10441fac825dceac5971eb30cbc
Frank Versnel <f.versnel@gmail.com>
2014-11-15 10:50:15 +0100

    Added Volatile class from jlanng/ImmutableCollectionsNet40

--- src/System.Collections.Immutable/src/System/Threading/Volatile.cs
+++ src/System.Collections.Immutable/src/System/Threading/Volatile.cs
@@ -1,147 +1,111 @@
-﻿using System.Security;
-
+﻿using System;
+using System.Runtime;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
 namespace System.Threading
 {
-    public static class Volatile
+    internal static class Volatile
     {
-        public static bool Read(ref bool location)
-        {
-            return false;
-        }
-
-        public static byte Read(ref byte location)
-        {
-            return (byte)0;
-        }
-
-        public static double Read(ref double location)
-        {
-            return 0.0;
-        }
-
         public static short Read(ref short location)
         {
-            return (short)0;
+            short result = location;
+            Thread.MemoryBarrier();
+            return result;
         }
 
-        public static int Read(ref int location)
-        {
-            return 0;
-        }
 
-        public static long Read(ref long location)
-        {
-            return 0L;
-        }
-
-        public static IntPtr Read(ref IntPtr location)
-        {
-            return new IntPtr();
-        }
-
-        [CLSCompliant(false)]
-        public static sbyte Read(ref sbyte location)
-        {
-            return (sbyte)0;
-        }
-
-        public static float Read(ref float location)
-        {
-            return 0.0f;
-        }
-
-        [CLSCompliant(false)]
-        public static ushort Read(ref ushort location)
-        {
-            return (ushort)0;
-        }
-
-        [CLSCompliant(false)]
-        public static uint Read(ref uint location)
-        {
-            return 0U;
-        }
-
-        [SecuritySafeCritical]
-        [CLSCompliant(false)]
-        public static ulong Read(ref ulong location)
-        {
-            return 0UL;
-        }
-
-        [CLSCompliant(false)]
         public static UIntPtr Read(ref UIntPtr location)
         {
-            return new UIntPtr();
+            UIntPtr result = location;
+            Thread.MemoryBarrier();
+            return result;
         }
-
-        [SecuritySafeCritical]
+        public static float Read(ref float location)
+        {
+            float result = location;
+            Thread.MemoryBarrier();
+            return result;
+        }
+        public static double Read(ref double location)
+        {
+            return Interlocked.CompareExchange(ref location, 0.0, 0.0);
+        }
         public static T Read<T>(ref T location) where T : class
         {
-            return default(T);
+            T result = location;
+            Thread.MemoryBarrier();
+            return result;
         }
-
         public static void Write(ref bool location, bool value)
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
-
-        public static void Write(ref byte location, byte value)
-        {
-        }
-
-        public static void Write(ref double location, double value)
-        {
-        }
-
-        public static void Write(ref short location, short value)
-        {
-        }
-
-        public static void Write(ref int location, int value)
-        {
-        }
-
-        public static void Write(ref long location, long value)
-        {
-        }
-
-        public static void Write(ref IntPtr location, IntPtr value)
-        {
-        }
-
-        [CLSCompliant(false)]
         public static void Write(ref sbyte location, sbyte value)
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
-
-        public static void Write(ref float location, float value)
+        public static void Write(ref byte location, byte value)
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
-
-        [CLSCompliant(false)]
+        public static void Write(ref short location, short value)
+        {
+            Thread.MemoryBarrier();
+            location = value;
+        }
         public static void Write(ref ushort location, ushort value)
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
-
-        [CLSCompliant(false)]
+        public static void Write(ref int location, int value)
+        {
+            Thread.MemoryBarrier();
+            location = value;
+        }
         public static void Write(ref uint location, uint value)
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
-
-        [CLSCompliant(false)]
-        [SecuritySafeCritical]
-        public static void Write(ref ulong location, ulong value)
+        public static void Write(ref long location, long value)
         {
+            Interlocked.Exchange(ref location, value);
         }
-
-        [CLSCompliant(false)]
+        //[CLSCompliant(false), ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success), TargetedPatchingOptOut("Performance critical to inline across NGen image boundaries"), SecuritySafeCritical]
+        //public unsafe static void Write(ref ulong location, ulong value)
+        //{
+        //    fixed (ulong* ptr = &location)
+        //    {
+        //        Interlocked.Exchange(ref *(long*)ptr, (long)value);
+        //    }
+        //}
+        public static void Write(ref IntPtr location, IntPtr value)
+        {
+            Thread.MemoryBarrier();
+            location = value;
+        }
         public static void Write(ref UIntPtr location, UIntPtr value)
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
-
-        [SecuritySafeCritical]
+        public static void Write(ref float location, float value)
+        {
+            Thread.MemoryBarrier();
+            location = value;
+        }
+        public static void Write(ref double location, double value)
+        {
+            Interlocked.Exchange(ref location, value);
+        }
         public static void Write<T>(ref T location, T value) where T : class
         {
+            Thread.MemoryBarrier();
+            location = value;
         }
     }
 }
